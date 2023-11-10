61452b3749802fcae15ccef27bcbf3a18b549768
eroist <97012368+eroist@users.noreply.github.com>
2023-11-10 10:00:54 -0500

    feat: update footer

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import xLogo from "../assets/images/x.svg";
+import shibaLogo from "../assets/images/shiba.svg";
 
 export default function Footer() {
   return (
-    <div className="bg-[#27262c] px-10 py-5 text-white">
-      Copyright © 2023 Zombium - All Rights Reserved.
+    <div className=" bg-[#27262c] px-10 py-10 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <span>Copyright © 2023 Zombium - All Rights Reserved.</span>
+        <div className="flex gap-5">
+          <a href="https://x.com/Zombium" target="_blank">
+            <img src={xLogo} className="w-8 h-8" alt="x" />
+          </a>
+          <a
+            href="https://www.shibariumscan.io/token/0xb1635A8a344aFC0bDc0e8cF26954815644Be7370"
+            target="_blank"
+          >
+            <img src={shibaLogo} className="w-8 h-8" alt="shiba" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
